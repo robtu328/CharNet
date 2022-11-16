fix normalization, add cross entropy loss calculation

charnet/modeling/postprocessing.py
@@ -289,7 +289,7 @@ class OrientedTextPostProcessing(nn.Module):
             text, score = decode(char_scores[order])
             return text, score, char_scores[order]
 
-        word_bbox_scores = word_bboxes[:, 8]
+        word_bbox_scores = word_bboxes[:, 8] #get box score
         char_bbox_scores = char_bboxes[:, 8]
         word_bboxes = word_bboxes[:, :8]
         char_bboxes = char_bboxes[:, :8]
@@ -301,6 +301,9 @@ class OrientedTextPostProcessing(nn.Module):
         num_char = char_bboxes.shape[0]
         word_instances = list()
         word_chars = [list() for _ in range(num_word)]
+        if num_word == 0:
+            print("No word box")
+            return word_instances
         for idx in range(num_char):
             char_bbox = char_bboxes[idx]
             char_poly = char_polys[idx]

data/data_loader.py
@@ -47,15 +47,16 @@ def collate_fn(batch):
     training_mask = []
     score_map_char = []
     geo_map_char = []
-    training_mask_char = []    
+    training_mask_char = [] 
+    polygon_chars = []
     
    
     for index in range(bs):
         #print ('index = ', index)
         if batch[index]['image'] is not None:
-            a = torch.from_numpy(batch[index]['image'])
-            a = a.permute(2, 1, 0)
-            #a = img[i]
+            #a = torch.from_numpy(batch[index]['image'])
+            #a = a.permute(2, 1, 0)
+            a = batch[index]['image']
             images.append(a)
             images_np.append(batch[index]['image'])
            
@@ -65,7 +66,7 @@ def collate_fn(batch):
             
             #c = torch.FloatTensor(batch[index]['polygons_char'])
             #c = c.permute(2, 0, 1)
-#            polygon_chars.append(batch[index]['polygons_char'])
+            polygon_chars.append(batch[index]['polygons_char'])
             
             #d = torch.from_numpy(batch[i]['lines_text'])
             lines_texts.append(batch[index]['lines_text'])
@@ -140,7 +141,7 @@ def collate_fn(batch):
 
     #return images, score_maps, geo_maps, training_masks
     #return images, polygons, polygon_chars, lines_texts, lines_chars, gts, masks, gt_chars, mask_chars, thresh_maps, thresh_masks, thresh_map_chars, thresh_mask_chars 
-    return images, score_map, geo_map, training_mask, score_map_char, geo_map_char, training_mask_char, images_np 
+    return images, score_map, geo_map, training_mask, score_map_char, geo_map_char, training_mask_char, images_np,  polygon_chars, lines_chars
 
 def default_collate(batch):
     r"""Puts each data field into a tensor with outer dimension batch size"""

data/data_utils.py
@@ -760,7 +760,7 @@ def generate_rbox(im_size, polys, tags, texts, WC='W', table=[]):
             new_p2 = line_cross_point(backward_opposite, edge_opposite)
             fitted_parallelograms.append([new_p0, new_p1, new_p2, new_p3, new_p0])
 
-        areas = [Polygon(t).area for t in fitted_parallelograms]
+        areas = [Polygon(t).area if t!=[] else print("Nulll Poly Area")for t in fitted_parallelograms]
         parallelogram = np.array(fitted_parallelograms[np.argmin(areas)][:-1], dtype=np.float32)
         # sort thie polygon
         parallelogram_coord_sum = np.sum(parallelogram, axis=1)

data/processes/normalize_image.py
@@ -1,21 +1,97 @@
 import numpy as np
 import torch
+import warnings
+from concern.config import State
 
 from .data_process import DataProcess
-
+from torchvision import transforms
 
 class NormalizeImage(DataProcess):
     RGB_MEAN = np.array([122.67891434, 116.66876762, 104.00698793])
+    norm_type = State(default="lib")
+ 
+    def __init__(self, cmd={}, *args, **kwargs):
+        self.load_all(cmd=cmd, **kwargs)
+        self.debug = False
+        warnings.simplefilter("ignore")
+        to_rgb_transform = transforms.Lambda(lambda x: x[[2, 1, 0]]) #RGB to BGR
+        
+        self.normalize = transforms.Normalize(mean = [ 0.485, 0.456, 0.406 ], std = [ 0.229, 0.224, 0.225 ])
+        
+        self.inv_normal = transforms.Compose([ 
+           transforms.Normalize(mean = [ 0., 0., 0. ], std = [ 1/0.229, 1/0.224, 1/0.225 ]), 
+           transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ], std = [ 1., 1., 1. ]), 
+           to_rgb_transform
+           ])
+        self.cnormalize = self.build_transform()
 
-    def process(self, data):
-        assert 'image' in data, '`image` in data is required by this process'
-        image = data['image']
+    def build_transform(self):
+        to_rgb_transform = transforms.Lambda(lambda x: x[[2, 1, 0]]) #BGR -> RGB
+
+        normalize_transform = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+
+        transform = transforms.Compose(
+            [
+               # transforms.ToPILImage(),
+                transforms.ToTensor(),    # HWC -> CHW
+                to_rgb_transform,         # BGR -> RGB     
+                normalize_transform,
+            ]
+        )
+        return transform        
+            
+    def lib_trans(self, image):
+        
+        return self.cnormalize(image)
+        #return self.normalize(torch.from_numpy(image).permute(2, 1, 0)).float()
+        
+    def lib_inv_trans(self, image):
+        
+        image1=(self.inv_normal(image.to('cpu')).numpy()*255).astype('uint8') 
+        
+        return np.ascontiguousarray(np.transpose(image1, (1,2,0)))
+        
+    def manual_trans(self, image):
+        
         image -= self.RGB_MEAN
         image /= 255.
         #image = torch.from_numpy(image).permute(2, 0, 1).float()
         image = torch.from_numpy(image).permute(2, 1, 0).float()
-        data['image'] = image
+        
+        return image
+    
+    def manual_inv_trans(self, image):
+        #image = image.permute(1, 2, 0).to('cpu').numpy()
+        image = image.permute(2, 1, 0).to('cpu').numpy()
+        image = image * 255.
+        image += self.RGB_MEAN
+        image = image.astype(np.uint8)       
+        
+        return image
+
+    def process(self, data):
+        
+        assert 'image' in data, '`image` in data is required by this process'
+        image = data['image']
+        
+        if(self.norm_type == 'lib'):
+            
+            data['image'] = self.lib_trans(image.astype('uint8'))
+        else:
+            data['image'] = self.manual_trans(image.astype('uint8'))
+        
         return data
+        #assert 'image' in data, '`image` in data is required by this process'
+        #image = data['image']
+        #image -= self.RGB_MEAN
+        #image /= 255.
+        #image = torch.from_numpy(image).permute(2, 0, 1).float()
+        #image = torch.from_numpy(image).permute(2, 1, 0).float()
+        #data['image'] = image
+        #return data
 
     @classmethod
     def restore(self, image):

tools/loss.py
@@ -7,6 +7,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import numpy as np
+
+import pyclipper
+from shapely.geometry import Polygon
+from charnet.modeling.postprocessing import load_char_dict
+
+def generalized_dice_loss_w(y_true, y_pred): 
+    # Compute weights: "the contribution of each label is corrected by the inverse of its volume"
+    Ncl = y_pred.shape[-1]
+    w = torch.zeros((Ncl,))
+    for l in range(0,Ncl): w[l] = torch.sum( torch.asarray(y_true[:,:,:,:,l]==1,torch.int8) )
+    w = 1/(w**2+0.00001)
+
+    # Compute gen dice coef:
+    numerator = y_true*y_pred
+    numerator = w*torch.sum(numerator,(0,1,2,3))
+    numerator = torch.sum(numerator)
+    
+    denominator = y_true+y_pred
+    denominator = w*torch.sum(denominator,(0,1,2,3))
+    denominator = torch.sum(denominator)
+    
+    gen_dice_coef = numerator/denominator
+    
+    return 1-2*gen_dice_coef
+
 
 
 
@@ -22,6 +48,7 @@ def dice_loss(true, logits, eps=1e-7):
         eps: added to the denominator for numerical stability.
     Returns:
         dice_loss: the Sørensen–Dice loss.
+        from: https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
     """
     num_classes = logits.shape[1]
     if num_classes == 1:
@@ -34,11 +61,13 @@ def dice_loss(true, logits, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
-    dims = (0,) + tuple(range(2, true.ndimension()))
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)] #add class dimension by using eye
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()  #move class to dimension 1
+        probas = logits
+        #probas = F.softmax(logits, dim=1) # suppress valuse below 1
+        #torch.where(probas>0.2, probas, 0.0)
+    true_1_hot = true_1_hot.type(logits.type()) #Change type to floatTensor
+    dims = (0,) + tuple(range(2, true.ndimension()+1)) # decide sum area
     intersection = torch.sum(probas * true_1_hot, dims)
     cardinality = torch.sum(probas + true_1_hot, dims)
     dice_loss = (2. * intersection / (cardinality + eps)).mean()
@@ -136,6 +165,121 @@ def Giou_np(bbox_p, bbox_g):
     return giou, loss_iou, loss_giou
 
 
+
+
+class char_matching(nn.Module):
+    def __init__(self, cfg):
+        super(char_matching, self).__init__()
+        self.char_dict_reverse = {}
+        self.char_dict = load_char_dict(cfg.CHAR_DICT_FILE)
+        self.num_class = len(self.char_dict)
+        for k, v in self.char_dict.items():
+            self.char_dict_reverse[v] = k 
+        self.loss = nn.CrossEntropyLoss()
+        self.debug = False
+        
+    def iou(self, bbox_p, bbox_g):
+        """
+        :param bbox_p: predict of bbox(N,4)(x1,y1,x2,y2)
+        :param bbox_g: groundtruth of bbox(N,4)(x1,y1,x2,y2)
+        :return:
+        """
+        # for details should go to https://arxiv.org/pdf/1902.09630.pdf
+        # ensure predict's bbox form
+        #     d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
+        d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = torch.split(bbox_g, 1, 1)
+        #     d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
+        d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = torch.split(bbox_p, 1, 1)
+    
+        #   d1 = Top, d2 = Bottom, d3 = Left, d4 = Right
+
+        area_gt = (d1_gt + d2_gt) * (d3_gt + d4_gt)
+        area_pred = (d1_pred + d2_pred) * (d3_pred + d4_pred)
+    
+        w_union = torch.min(d3_gt, d3_pred) + torch.min(d4_gt, d4_pred)
+        h_union = torch.min(d1_gt, d1_pred) + torch.min(d2_gt, d2_pred)
+        I = w_union * h_union
+        U = area_gt + area_pred - I
+    
+        # calc area of Bc
+
+        U = area_pred + area_gt - I
+        #iou = 1.0 * I / U
+
+        return I, U, theta_pred, theta_gt    
+        
+    def forward(self, char_bboxes, char_scores, polygon_chars, line_chars):
+    
+        
+        match_all=[]
+        
+        for pic_idx in range(len(polygon_chars)):    
+            maxchar_score = np.argmax(char_scores[pic_idx], 1)
+            
+            match_idx=np.empty(len(polygon_chars[pic_idx]))
+            match_idx[:]=None
+            for gidx in range(len(polygon_chars[pic_idx])):
+                inter_area=np.zeros(len(char_bboxes[pic_idx]))
+                
+                
+                ppoly=pyclipper.scale_to_clipper(polygon_chars[pic_idx][gidx].reshape((4, 2)))
+                
+                for pidx in range(len(char_bboxes[pic_idx])):
+                    gpoly=pyclipper.scale_to_clipper(char_bboxes[pic_idx][pidx][:8].reshape((4, 2)))
+                    
+                    pc = pyclipper.Pyclipper()
+                    pc.AddPath(ppoly, pyclipper.PT_CLIP, True)
+                    pc.AddPaths([gpoly], pyclipper.PT_SUBJECT, True)
+                    solution = pc.Execute(pyclipper.CT_INTERSECTION)
+                    
+                    if len(solution) == 0:
+                        inter = 0
+                    else:
+                        inter = pyclipper.scale_from_clipper(
+                            pyclipper.scale_from_clipper(pyclipper.Area(solution[0])))
+                        
+                        inter_area[pidx]=inter
+                     
+                gmax_idx= np.argmax(inter_area) #Find the most match golden box's index, inter_area len=golden 
+
+                if(inter_area[gmax_idx] != 0):
+                    match_idx[gidx] = gmax_idx
+                    
+            if(self.debug):print (match_idx)
+            pred_line=[]
+            pred_number=[]
+            for gidx in range(len(polygon_chars[pic_idx])):
+                pidx_char = None
+                if(not np.isnan(match_idx[gidx])):
+                    if(self.debug):
+                        print('gidx = ', gidx, "match_idx[gidx] = ", match_idx[gidx])
+                        print("Maxcharscore ", maxchar_score[match_idx[gidx].astype('uint')] )
+                    pred_line_chars= self.char_dict[maxchar_score[match_idx[gidx].astype('uint')].astype('uint')]
+                    golden_char=line_chars[pic_idx][gidx]
+                    pred_line.append(pred_line_chars)
+                    pred_number.append(maxchar_score[match_idx[gidx].astype('int')].astype('int'))
+                else:
+                    pred_line.append(" ")
+                    pred_number.append(-1)
+            
+            golden_class=[]
+            for k in line_chars[pic_idx]:
+                golden_class.append(self.char_dict_reverse[k.upper()])     
+            golden_class_onehot=torch.eye(self.num_class)[golden_class]
+            
+            
+            pred_class_score=char_scores[pic_idx][match_idx.astype('uint8')]
+            
+            
+            
+            indices=np.where (np.array(pred_number) == -1)[0]
+            pred_class_onehot = torch.eye(self.num_class)[pred_number]
+            pred_class_onehot[indices] = 0
+            match_all.append(match_idx)
+            
+                
+    
+        print(match_all)
 
 class LossFunc(nn.Module):
     def __init__(self, losstype='iou'):

tools/train_net.py
@@ -87,19 +87,194 @@ def params_gen( net):
                     'weight_decay': 0.0001
                 }]
     return params
-def train_model( args, cfg, img_loader ):
+def train_model( charnet, args, cfg, img_loader, train_cfg, debug=False):
     
 
+    epochs=train_cfg['epochs']
     charnet = CharNet()
     charnet.load_state_dict(torch.load(cfg.WEIGHT))
-    charnet.eval()
+    #charnet.eval()
     charnet.train()
     charnet.cuda()
     params=params_gen(charnet)
+    #params1=params_gen(charnet.backbone) + params_gen(charnet.word_detector)
+    #params2=params_gen(charnet.backbone) + params_gen(charnet.char_detector)
+    #params3=params_gen(charnet.backbone) + params_gen(charnet.char_recognizer)
+    
     
     optimizer = torch.optim.SGD(params, momentum=0.001)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.94)
+    #optimizer1 = torch.optim.SGD(params1, momentum=0.001)
+    #scheduler1 = lr_scheduler.StepLR(optimizer1, step_size=10000, gamma=0.94)
+    #optimizer2 = torch.optim.SGD(params2, momentum=0.001)
+    #scheduler2 = lr_scheduler.StepLR(optimizer2, step_size=10000, gamma=0.94)
+    #optimizer3 = torch.optim.SGD(params3, momentum=0.001)
+    #scheduler3 = lr_scheduler.StepLR(optimizer3, step_size=10000, gamma=0.94)
+
     criterion = LossFunc()
+    cmatch= char_matching(cfg)
+    
+    #sequence=iter(img_loader)
+    #images, polygons, polygon_chars, lines_texts, lines_chars, gts, ks, gt_chars, mask_chars, thresh_maps, thresh_masks, thresh_map_chars, thresh_mask_chars=next(sequence)
+    #batch=next(sequence)
+    #default_collate(batch)
+    back_batch_time = 5
+    batch_times = 0
+    loss_all = 0
+    
+#    for batch in img_loader:
+#        for ind in range(len(batch['image'])):
+    time=0
+#    for (images, polygons, polygon_chars, lines_texts, lines_chars, gts, ks, gt_chars, mask_chars) in img_loader:
+
+    for eidx in range (train_synth_cfg['epochs']):
+        loss1_total = 0
+        loss2_total = 0
+        loss3_total = 0
+        iter_cnt = 0
+        for (images, score_map, geo_map, training_mask, score_map_char, geo_map_char, training_mask_char, images_np, polygon_chars, line_chars) in img_loader: 
+            char_bboxes, char_scores, word_instances, pred_word_fg, pred_word_tblr,\
+                pred_word_orient, pred_char_fg, pred_char_tblr, pred_char_orient, pred_char_cls \
+                    = charnet(images.cuda(), 1, 1, images[0].size()[1], images[0].size()[2], images_np)
+            
+            
+            #pred_word_tblr = torch.permute(pred_word_tblr, (0, 2, 3, 1))
+            #pred_char_tblr = torch.permute(pred_char_tblr, (0, 2, 3, 1))
+            char_size = pred_char_tblr.size()
+            #pred_word_orient = torch.permute(pred_word_orient, (0, 2, 3, 1))
+            pred_char_orient = torch.zeros([char_size[0], 1, char_size[2], char_size[3]]).cuda()
+        
+            pred_word_tblra = torch.cat((pred_word_tblr, pred_word_orient), 1)
+            pred_char_tblra = torch.cat((pred_char_tblr, pred_char_orient), 1)
+            
+            pred_word_fg_sq = torch.squeeze(pred_word_fg[:,1::], 1)
+            pred_char_fg_sq = torch.squeeze(pred_char_fg[:,1::], 1)
+            
+            score_map_char_mask=torch.where(score_map_char> 0, 1, 0)  
+            score_map_char_adjust=torch.where(score_map_char> 0, -1, 0) 
+            score_map_char = score_map_char + score_map_char_adjust
+            
+            score_map=score_map.cuda()
+            geo_map=torch.permute(geo_map.cuda(), (0,3,1,2))
+            training_mask=training_mask.cuda()
+            score_map_char_mask=score_map_char_mask.cuda()
+            score_map_mask=score_map_char.cuda()
+            geo_map_char=torch.permute(geo_map_char[0].cuda(), (0,3,1,2))
+            training_mask_char=training_mask_char.cuda()    
+                    
+            loss1 = criterion(score_map, pred_word_fg_sq, geo_map, pred_word_tblra, training_mask)
+            loss2 = criterion(score_map_char_mask, pred_char_fg_sq, geo_map_char, pred_char_tblra, training_mask_char)
+            loss3 = dice_loss(score_map_char, pred_char_cls*score_map_char_mask.unsqueeze(1))
+            loss4 = cmatch(char_bboxes, char_scores, polygon_chars, line_chars)
+ 
+            loss1_total = loss1_total + loss1
+            loss2_total = loss2_total + loss2
+            loss3_total = loss3_total + loss3
+            
+            weighted1=0.3
+            weighted2=0.3
+            weighted3=0.4
+            #loss_all = loss1 + loss2 + loss3
+            loss_all = loss1*weighted1 + loss2*weighted2 + loss3*weighted3
+            print ("Loss all: ", loss_all, "loss1: ", loss1, "loss2: ", loss2, "loss3: ", loss3)
+            #scheduler.step()
+
+            loss_all=loss_all / back_batch_time
+            loss_all.backward()
+            
+            
+            #loss1=loss1 / back_batch_time
+            #loss2=loss2 / back_batch_time
+            #loss3=loss3 / back_batch_time
+            
+            #loss1.backward()
+            #loss2.backward()
+            #loss3.backward()
+            batch_times = batch_times + 1
+        
+            if batch_times >= back_batch_time:
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                
+ #               optimizer1.step()
+ #               scheduler1.step()
+ #               optimizer1.zero_grad()
+ #               optimizer2.step()
+ #               scheduler2.step()
+ #               optimizer2.zero_grad()            
+ #               optimizer3.step()
+ #               scheduler3.step()
+ #               optimizer3.zero_grad()            
+                batch_times = 0
+        
+
+        
+            iter_cnt = iter_cnt + 1
+            time = time + 1
+            if debug==True and time == 10:
+                exit()
+
+
+
+
+            state = {
+                    'epochs'      : epochs,
+                    'state_dict' : charnet.state_dict(),
+                    'optimizer'  : optimizer.state_dict(),
+ #                   'optimizer1'  : optimizer1.state_dict(),
+ #                   'optimizer2'  : optimizer2.state_dict(),
+ #                   'optimizer3'  : optimizer3.state_dict()
+                    }
+            torch.save(state, './model_save.pth')
+            #save_checkpoint(state, epoch)
+
+        loss1_average = loss1_total / iter_cnt
+        loss2_average = loss2_total / iter_cnt
+        loss3_average = loss3_total / iter_cnt
+        loss_average = loss1_average*weighted1 + loss2_average*weighted2 + loss3_average*weighted3
+        print ("Loss average all: ", loss_average, "loss1_average: ", loss1_average, "loss2_average: ", loss2_average, "loss3_average: ", loss3_average)
+        #for ind in range(len(images)):
+        #    char_bboxes, char_scores, word_instances = charnet(images[ind], 1, 1, images[ind].size()[0], images[ind].size()[1])
+#            char_bboxes, char_scores, word_instances = charnet(batch['image'][ind].numpy().astype('uint8'), 1, 1, batch['image'][ind].size()[0], batch['image'][ind].size()[1])
+        #    print(char_bboxes, word_instances)
+        
+        
+        #Check  Image size. and transofrmation reuslt
+#        print("Processing {}...".format(im_name))
+#        im_file = os.path.join(args.image_dir, im_name)
+#        im_original = cv2.imread(im_file)
+#        im, scale_w, scale_h, original_w, original_h = resize(im_original, size=cfg.INPUT_SIZE)
+#        with torch.no_grad():b
+#            char_bboxes, char_scores, word_instances = charnet(im, scale_w, scale_h, original_w, original_h)
+#            save_word_recognition(
+#                word_instances, os.path.splitext(im_name)[0],
+#                args.results_dir, cfg.RESULTS_SEPARATOR
+#            )
+
+    
+    
+    
+    params=params_gen(charnet)
+    
+    return params
+
+
+
+def test_model( charnet, args, cfg, img_loader, debug=False ):
+    
+
+    #charnet = CharNet()
+    #charnet.load_state_dict(torch.load(cfg.WEIGHT))
+    
+    charnet.eval()
+    charnet.cuda()
+    params=params_gen(charnet)
+    
+    #optimizer = torch.optim.SGD(params, momentum=0.001)
+    #scheduler = lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.94)
+    criterion = LossFunc()
+    cmatch= char_matching(cfg)
     
     #sequence=iter(img_loader)
     #images, polygons, polygon_chars, lines_texts, lines_chars, gts, ks, gt_chars, mask_chars, thresh_maps, thresh_masks, thresh_map_chars, thresh_mask_chars=next(sequence)
@@ -110,6 +285,7 @@ def train_model( args, cfg, img_loader ):
 #    for batch in img_loader:
 #        for ind in range(len(batch['image'])):
     time=0
+    back_batch_time = 4
 #    for (images, polygons, polygon_chars, lines_texts, lines_chars, gts, ks, gt_chars, mask_chars) in img_loader:
     for (images, score_map, geo_map, training_mask, score_map_char, geo_map_char, training_mask_char, images_np) in img_loader: 
         char_bboxes, char_scores, word_instances, pred_word_fg, pred_word_tblr,\
@@ -145,17 +321,21 @@ def train_model( args, cfg, img_loader ):
         loss2 = criterion(score_map_char_mask, pred_char_fg_sq, geo_map_char, pred_char_tblra, training_mask_char)
         loss3 = dice_loss(score_map_char, pred_char_cls)
  
-        loss_all = loss1 + loss2 + loss3
+        weighted1=0.3
+        weighted2=0.3
+        weighted3=0.4
+        #loss_all = loss1 + loss2 + loss3
+        loss_all = loss1*weighted1 + loss2*weighted2 + loss3*weighted3
         print ("Loss all: ", loss_all, "loss1: ", loss1, "loss2: ", loss2, "loss3: ", loss3)
         #scheduler.step()
-        optimizer.zero_grad()
-        loss_all.backward()
-        optimizer.step()
-        scheduler.step()
+        #optimizer.zero_grad()
+        #loss_all.backward()
+        #optimizer.step()
+        #scheduler.step()
         
         
         time = time + 1
-        if time == 10:
+        if debug==True and time == 10:
             exit()
 
         #for ind in range(len(images)):
@@ -181,10 +361,14 @@ def train_model( args, cfg, img_loader ):
     
     
     
-    return optimizer, params
+    return  params
 
 
-def test_model(args, cfg):
+
+
+
+
+def test_model2(args, cfg):
     
     charnet = CharNet()
     charnet.load_state_dict(torch.load(cfg.WEIGHT))
@@ -236,23 +420,32 @@ if __name__ == '__main__':
     
     
     
-    myprocess=train_synth_img_loader.data_loader.dataset.processes
+    myprocess=train_synth_img_loader.data_loader.dataset.processes;
+    #myprocess=train_synth_img_loader.data_loader.dataset[22]
     data = {}
-    image_path=train_synth_img_loader.data_loader.dataset.image_paths[1]
-    target = train_synth_img_loader.data_loader.dataset.targets[1]
-    target_char = train_synth_img_loader.data_loader.dataset.targets_char[1]
-    img = cv2.imread(image_path, cv2.IMREAD_COLOR).astype('float32')
+    image_path=train_synth_img_loader.data_loader.dataset.image_paths[973]
+    #target = train_synth_img_loader.data_loader.dataset.targets[1]
+    #target_char = train_synth_img_loader.data_loader.dataset.targets_char[1]
+    data=train_synth_img_loader.data_loader.dataset.getData(973)
+    
+    imgr = cv2.imread(image_path, cv2.IMREAD_COLOR).astype('float32')
+    #imgr = cv2.imread(image_path, cv2.IMREAD_COLOR)
     data['filename']=image_path
     data['data_id']=image_path
-    data['image']=img
-    data['lines']=target
-    data['chars']=target_char
+    data['image']=imgr
+    
+    #data5=train_synth_img_loader.data_loader.dataset[1]
+    #data['lines']=target
+    #data['chars']=target_char
+    #img_pathd="./test.jpg"
+    #imgd=cv2.imread(img_pathd)
+    
     data1=myprocess[0](data)
     data2=myprocess[1](data1)
     data3=myprocess[2](data2)
     data4=myprocess[3](data3)
     data5=myprocess[4](data4)
-    #data6=myprocess[5](data5)
+    data6=myprocess[5](data5)
     
     #dlen = len(train_synth_img_loader.data_loader)
     #print ('Len = ', dlen)
@@ -269,11 +462,11 @@ if __name__ == '__main__':
     #data7 = myconv(data6['image'])
     #data7=myprocess[6](data6)
     
-    #0 <data.processes.augment_data.AugmentDetectionData >, 
-    #1 <data.processes.random_crop_data.RandomCropData >, 
-    #2 <data.processes.make_icdar_data.MakeICDARData >, 
+    #0 <data.processes.augment_data.AugmentDetectionData >,    -> affine transforer, Flip, resize
+    #1 <data.processes.random_crop_data.RandomCropData >,      -> Random Crop Data
+    #2 <data.processes.make_icdar_data.MakeICDARData >,        -> Bonding box transfer to t,b,l,r, angle
     #3 <data.processes.make_seg_detection_data.MakeSegDetectionData >, remove 
-    #4->3 <data.processes.make_border_map.MakeBorderMap >, 
+    #4->3 <data.processes.make_border_map.MakeBorderMap >,     -> Shrink Bonding box and bonding box generation 
     #5->4 <data.processes.normalize_image.NormalizeImage >, 
     #6->5 <data.processes.filter_keys.FilterKeys >
     
@@ -292,16 +485,17 @@ if __name__ == '__main__':
     #cv2.imshow('TEST', dst.astype('uint8'))
     #cv2.waitKey(0)
     
-    
+    charnet = CharNet()
+    charnet.load_state_dict(torch.load(cfg.WEIGHT))
     #Trainsetting(Trainsetting_conf['Experiment']['train']])
 #Train without profile
-    #train_model(args, cfg, train_synth_img_loader.data_loader)
+    train_model(charnet, args, cfg, train_synth_img_loader.data_loader, train_synth_cfg)
 #Train code with profiling
-    cProfile.run('train_model(args, cfg, train_synth_img_loader.data_loader)', 'restats')
-    p = pstats.Stats('restats')
-    p.sort_stats(SortKey.TIME).print_stats(50)
-    p.sort_stats(SortKey.CUMULATIVE).print_stats(50)
-    #train_model(args, cfg, train_synth_img_loader.data_loader)
+    #cProfile.run('train_model(args, cfg, train_synth_img_loader.data_loader)', 'restats')
+    #p = pstats.Stats('restats')
+    #p.sort_stats(SortKey.TIME).print_stats(50)
+    #p.sort_stats(SortKey.CUMULATIVE).print_stats(50)
+    
 
 #TEST code
     #test_model(args, cfg)