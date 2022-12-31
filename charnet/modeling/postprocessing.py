add loss 5 calculation fix None result in rbox generation.

charnet/modeling/postprocessing.py
@@ -233,12 +233,13 @@ class OrientedTextPostProcessing(nn.Module):
         keep, new_oriented_char_bboxes, new_char_scores = nms_with_char_cls_torch(
             oriented_char_bboxes, char_scores, self.char_nms_iou_thresh, num_neig=1
         )
+        print("Char keep len = ", len(keep))
         new_oriented_char_bboxes = new_oriented_char_bboxes[keep]
         new_oriented_char_bboxes[:, :8] = new_oriented_char_bboxes[:, :8].round()
         new_oriented_char_bboxes[:, 0:8:2] = np.maximum(0, np.minimum(W-1, new_oriented_char_bboxes[:, 0:8:2]))
         new_oriented_char_bboxes[:, 1:8:2] = np.maximum(0, np.minimum(H-1, new_oriented_char_bboxes[:, 1:8:2]))
-        new_char_scores1 = new_char_scores[keep].clone()
-
+        
+        new_char_scores1 = new_char_scores[keep]
         new_oriented_char_bboxes1 = new_oriented_char_bboxes.copy()        
         new_char_scores=None
         new_oriented_char_bboxes=None
@@ -253,7 +254,61 @@ class OrientedTextPostProcessing(nn.Module):
         
         return new_oriented_char_bboxes1, new_char_scores1
 
+    def parse_char_in_gt(
+            self, pred_word_fg, pred_char_fg,
+            pred_char_tblr, pred_char_cls,
+            score_map_mask, geo_map_char, score_map_char, 
+            scale_w, scale_h, W, H
+    ):
 
+        char_boxes_pic=[]
+        char_scores_pic=[]
+        score_map_keep_pic=[]
+        char_stride = self.char_stride 
+        
+        
+        for pidx in range(score_map_mask.shape[0]):
+            
+            char_keep_rows, char_keep_cols = np.where(score_map_mask[pidx] > 0)
+
+            oriented_char_bboxes = np.zeros((char_keep_rows.shape[0], 9), dtype=np.float32)
+            char_scores = torch.zeros(char_keep_rows.shape[0], self.num_char_class)
+            score_map_char_keep = np.zeros(char_keep_rows.shape[0], dtype=np.int32)
+        
+            for idx in range(oriented_char_bboxes.shape[0]):
+                y, x = char_keep_rows[idx], char_keep_cols[idx]
+                t, b, l, r = pred_char_tblr[pidx][:, y, x].detach().cpu().numpy()
+                o = 0.0  # pred_char_orient[y, x]
+                score = pred_char_fg[pidx][1][y, x]
+                four_points = rotate_rect(
+                    scale_w * char_stride * (x-l), scale_h * char_stride * (y-t),
+                    scale_w * char_stride * (x+r), scale_h * char_stride * (y+b),
+                    o, scale_w * char_stride * x, scale_h * char_stride * y)
+                oriented_char_bboxes[idx, :8] = np.array(four_points, dtype=np.float32).flat
+                oriented_char_bboxes[idx, 8] = score
+                char_scores[idx, :] = pred_char_cls[pidx][:, y, x]
+                score_map_char_keep [idx]=score_map_char[pidx][y,x]
+            #keep, new_oriented_char_bboxes, new_char_scores = nms_with_char_cls_torch(
+            #    oriented_char_bboxes, char_scores, self.char_nms_iou_thresh, num_neig=1
+            #    )
+            print("Char keep len = ", len(char_keep_rows))
+        
+            oriented_char_bboxes[:, :8] = oriented_char_bboxes[:, :8].round()
+            oriented_char_bboxes[:, 0:8:2] = np.maximum(0, np.minimum(W-1, oriented_char_bboxes[:, 0:8:2]))
+            oriented_char_bboxes[:, 1:8:2] = np.maximum(0, np.minimum(H-1, oriented_char_bboxes[:, 1:8:2]))
+        
+            char_scores_pic.append(char_scores)
+            char_boxes_pic.append(oriented_char_bboxes.copy()) 
+            score_map_keep_pic.append(score_map_char_keep)
+            
+            char_scores=None
+            oriented_char_bboxes=None
+            
+#        del pred_char_cls
+#        gc.collect()
+#        torch.cuda.empty_cache()
+        
+        return char_boxes_pic, char_scores_pic
 
     def filter_word_instances(self, word_instances, lexicon):
         def match_lexicon(text, lexicon):

data/data_utils.py
@@ -693,6 +693,7 @@ def generate_rbox(im_size, polys, tags, texts, WC='W', table=[]):
         # if geometry == 'RBOX':
         # 对任意两个顶点的组合生成一个平行四边形
         fitted_parallelograms = []
+        #print("Poly, ", poly)
         for i in range(4):
             p0 = poly[i]
             p1 = poly[(i + 1) % 4]
@@ -759,8 +760,18 @@ def generate_rbox(im_size, polys, tags, texts, WC='W', table=[]):
             new_p1 = line_cross_point(backward_opposite, edge)
             new_p2 = line_cross_point(backward_opposite, edge_opposite)
             fitted_parallelograms.append([new_p0, new_p1, new_p2, new_p3, new_p0])
-
-        areas = [Polygon(t).area if t!=[] else print("Nulll Poly Area")for t in fitted_parallelograms]
+            
+        #print(fitted_parallelograms)
+        idx = 0
+        #print (' Len =', len(fitted_parallelograms))
+        for t in fitted_parallelograms:
+            if t==[]:
+                print("Nulll Poly Area")
+            elif any(elem is None for elem in t):
+                print("None in t")        
+            #print ("Index (", idx,")", t)
+            idx=idx+1
+        areas = [Polygon(t).area if t!=[] and all(elem is not None for elem in t) else 65536.0 for t in fitted_parallelograms]
         parallelogram = np.array(fitted_parallelograms[np.argmin(areas)][:-1], dtype=np.float32)
         # sort thie polygon
         parallelogram_coord_sum = np.sum(parallelogram, axis=1)

tools/train_net.py
@@ -108,7 +108,7 @@ def train_model( charnet, args, cfg, img_loader, train_cfg, debug=False):
     optimizer = torch.optim.SGD(params,lr=0.05, momentum=0.9)
     scheduler = lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.94)
 
-    criterion = LossFunc()
+    criterion = LossFunc() 
     cmatch= char_matching(cfg)
     
     #sequence=iter(img_loader)
@@ -153,19 +153,36 @@ def train_model( charnet, args, cfg, img_loader, train_cfg, debug=False):
             score_map_char_mask=torch.where(score_map_char> 0, 1, 0)  
             score_map_char_adjust=torch.where(score_map_char> 0, -1, 0) 
             score_map_char = score_map_char + score_map_char_adjust
+                        
+            
             
             score_map=score_map.cuda()
             geo_map=torch.permute(geo_map.cuda(), (0,3,1,2))
             training_mask=training_mask.cuda()
             score_map_char_mask=score_map_char_mask.cuda()
             score_map_mask=score_map_char.cuda()
+            score_map_mask_np=score_map_char_mask.cpu().numpy()
+            
             geo_map_char=torch.permute(geo_map_char[0].cuda(), (0,3,1,2))
             training_mask_char=training_mask_char.cuda()    
                     
+            
+            
+            #char_bboxes_loss, char_scores_loss = charnet.post_processing.parse_char_in_gt(
+            #    pred_word_fg, pred_char_fg, pred_char_tblr, pred_char_cls,
+            #    score_map_mask_np, geo_map_char, score_map_char, 
+            #    1, 1, images[0].size()[1], images[0].size()[2]
+            #)
+            
+            
             loss1 = criterion(score_map, pred_word_fg_sq, geo_map, pred_word_tblra, training_mask)
             loss2 = criterion(score_map_char_mask, pred_char_fg_sq, geo_map_char, pred_char_tblra, training_mask_char)
             #loss3 = dice_loss(score_map_char, pred_char_cls*score_map_char_mask.unsqueeze(1))
             #loss3 = dice_loss(score_map_char, pred_char_cls*score_map_char_mask.unsqueeze(1))
+            
+            loss5 = keep_ce_loss(pred_char_fg, pred_char_cls, score_map_mask_np, score_map_char)
+            pred_char_fg, pred_char_cls,
+            score_map_mask, score_map_char
             
             if debug == True:
                 print ("Memory check start")
@@ -200,9 +217,10 @@ def train_model( charnet, args, cfg, img_loader, train_cfg, debug=False):
             weighted2=0.3
             weighted3=0.4
             weighted4=1.0
+            weighted5=0.8
             #loss_all = loss1 + loss2 + loss3
-            loss_all = loss1*weighted1 + loss2*weighted2 + (loss4)*weighted4
-            print ("No:", iter_cnt, ", Loss all: ", loss_all, "loss1: ", loss1, "loss2: ", loss2, "loss3: ", loss3, "loss4:", loss4, "accuracy:", correct_number/total_number)
+            loss_all = loss1*weighted1 + loss2*weighted2 + (loss5)*weighted5
+            print ("No:", iter_cnt, ", Loss all: ", loss_all, "loss1: ", loss1, "loss2: ", loss2, "loss3: ", loss3, "loss4:", loss4, "loss5:", loss5, "accuracy:", correct_number/total_number)
             #scheduler.step()
 
             loss_all=loss_all / back_batch_time